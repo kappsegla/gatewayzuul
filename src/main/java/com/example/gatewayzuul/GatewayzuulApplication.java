bf0395e4edec55969f822ed6daecbb5c1931f3d4
Martin Blomberg <martin.blomberg@outlook.com>
2019-02-26 11:17:15 +0100

    Initial commit

---
+++ src/main/java/com/example/gatewayzuul/GatewayzuulApplication.java
@@ -0,0 +1,13 @@
+package com.example.gatewayzuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayzuulApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayzuulApplication.class, args);
+    }
+
+}
